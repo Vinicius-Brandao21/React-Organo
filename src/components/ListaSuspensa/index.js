f3361e90eb2b0c80b55ca1ef6912d0ae233df602
Vinicius Brandão <https://github.com/Vinicius-Brandao21>
2024-08-13 13:00:04 -0300

    Termino do Organo

---
+++ src/components/ListaSuspensa/index.js
@@ -0,0 +1,16 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) =>{
+    //console.log(props.itens)
+    return(
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} value={props.valor} required={props.obrigatorio}>
+                    <option value=''></option>
+                    {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
